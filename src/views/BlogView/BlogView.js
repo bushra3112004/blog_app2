bbb084e589f46f0c365dbfed9ca2e873b3c301fc
bushra3112004 <pathanbushra006@gmail.com>
2024-06-24 13:02:41 -0700

    add sentence

---
+++ src/views/BlogView/BlogView.js
@@ -0,0 +1,21 @@
+import "./BlogView.css";
+import { useParams } from "react-router-dom";
+import blogs from "../../data";
+
+function BlogView() {
+    const { id } = useParams();
+    const selectedBlog = blogs.find((blogObject) => blogObject.id === id);
+    if (selectedBlog) {
+        return <div>Blog not found</div>;
+    }
+    return (
+        <div>
+            <h1>{selectedBlog.title}</h1>
+            <p>
+                <img src={selectedBlog.city.picture} alt="Blog-City" />
+            </p>
+            <div>{selectedBlog.content}</div>
+        </div>
+    )
+}
+export default BlogView;
